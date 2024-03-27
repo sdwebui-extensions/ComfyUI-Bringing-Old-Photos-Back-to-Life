sdwebui-extensions/ComfyUI-Bringing-Old-Photos-Back-to-Life

commit 03bc692ef33997b806bb521f3af4262f02149b31

Fix model weight links in README.

Face_Detection/align_warp_back_multiple_dlib.py
@@ -482,8 +482,8 @@ def main(checkpoint_path: str, origin_url: str, replace_url: str, save_url: str,
                 enhanced_face = np.array(enhanced_face)
 
             # convert colorspace
-            A = cv2.cvtColor(aligned_face.astype("uint8"), cv2.COLOR_RGB2BGR)
-            B = cv2.cvtColor(enhanced_face.astype("uint8"), cv2.COLOR_RGB2BGR)
+            A = cv2.cvtColor(A.astype("uint8"), cv2.COLOR_RGB2BGR)
+            B = cv2.cvtColor(B.astype("uint8"), cv2.COLOR_RGB2BGR)
             B = match_histograms(B, A)  ## histogram color matching
             enhanced_face = cv2.cvtColor(B.astype("uint8"), cv2.COLOR_BGR2RGB)
 

commit 1580f67c3ad2aba4f86f7a3bc40d611ffa591244

Fix bug introduced in 03bc692ef33997b806bb521f3af4262f02149b31

Face_Detection/align_warp_back_multiple_dlib.py
@@ -482,8 +482,8 @@ def main(checkpoint_path: str, origin_url: str, replace_url: str, save_url: str,
                 enhanced_face = np.array(enhanced_face)
 
             # convert colorspace
-            A = cv2.cvtColor(A.astype("uint8"), cv2.COLOR_RGB2BGR)
-            B = cv2.cvtColor(B.astype("uint8"), cv2.COLOR_RGB2BGR)
+            A = cv2.cvtColor(aligned_face.astype("uint8"), cv2.COLOR_RGB2BGR)
+            B = cv2.cvtColor(enhanced_face.astype("uint8"), cv2.COLOR_RGB2BGR)
             B = match_histograms(B, A)  ## histogram color matching
             enhanced_face = cv2.cvtColor(B.astype("uint8"), cv2.COLOR_BGR2RGB)
 